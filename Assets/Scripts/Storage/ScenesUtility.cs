a2d051003dedd8fe837df1be01b0071e81127755
JanZinch <jan.zinchanka@mail.ru>
2024-01-08 17:14:22 +0300

    refactor

--- Assets/Scripts/Storage/ScenesUtility.cs
+++ Assets/Scripts/Storage/ScenesUtility.cs
@@ -1,6 +1,6 @@
 ﻿namespace Storage
 {
-    public class ScenesUtility
+    public static class ScenesUtility
     {
         public const int StartSceneIndex = 0;
         public const int GameSceneIndex = 1;
