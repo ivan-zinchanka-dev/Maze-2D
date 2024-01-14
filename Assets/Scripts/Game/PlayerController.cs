64ef40fc80f0e67ace5cda4eb0898afb92fd6732
JanZinch <jan.zinchanka@mail.ru>
2024-01-14 19:19:03 +0300

    fix

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using Maze;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Game
 {
@@ -16,23 +15,7 @@ namespace Game
         private bool _canMove = true;
         
         public SpriteRenderer SpriteRenderer => _spriteRenderer;
-        
-        
-        
-        /*private Vector2Int WorldToMapCoords(Vector2 source) {
 
-            if (source.y < 0)
-            {
-                if (source.x > 0) return _currentPosInMap = new Vector2Int(_map.GetLength(0) / 2 + 1, _map.GetLength(1) / 2 - 1);
-                else return _currentPosInMap = new Vector2Int(_map.GetLength(0) / 2, _map.GetLength(1) / 2 - 1);
-            }
-            else
-            {
-                if (source.x > 0) return _currentPosInMap = new Vector2Int(_map.GetLength(0) / 2 + 1, _map.GetLength(1) / 2);
-                else return _currentPosInMap = new Vector2Int(_map.GetLength(0) / 2, _map.GetLength(1) / 2);
-            }        
-        }*/
-        
         private Vector2Int WorldToMapCoords(Vector2 source) {
 
             Vector2Int mapCoords = new Vector2Int(_map.GetLength(0) / 2, _map.GetLength(1) / 2);
@@ -79,35 +62,41 @@ namespace Game
             yield return null;    
         }
 
+        private void StartMakingStep(Vector3 target)
+        {
+            StartCoroutine(MakeStep(target, _speed));
+        }
 
         private void Update()
         {
             if (!_canMove || GameManager.Instance.Pause) return;
 
-            if (_currentPosInMap.x >= _map.GetLength(0) || _currentPosInMap.y >= _map.GetLength(1))
+            if (_currentPosInMap.x < 0 || _currentPosInMap.x >= _map.GetLength(0) ||
+                _currentPosInMap.y < 0 || _currentPosInMap.y >= _map.GetLength(1))
             {
                 GameManager.Instance.RegenerateMaze();
             }
             else if (Input.GetButton("Left") && !_map[_currentPosInMap.x, _currentPosInMap.y].HasFlag(WallState.Left))
             {
-                StartCoroutine(MakeStep(new Vector2(transform.position.x - _mapCellSize, transform.position.y), _speed));
+                StartMakingStep(new Vector2(transform.position.x - _mapCellSize, transform.position.y));
                 _currentPosInMap.x--;
             }
             else if (Input.GetButton("Right") && !_map[_currentPosInMap.x, _currentPosInMap.y].HasFlag(WallState.Right))
             {
-                StartCoroutine(MakeStep(new Vector2(transform.position.x + _mapCellSize, transform.position.y), _speed));
+                StartMakingStep(new Vector2(transform.position.x + _mapCellSize, transform.position.y));
                 _currentPosInMap.x++;
             }
             else if (Input.GetButton("Up") && !_map[_currentPosInMap.x, _currentPosInMap.y].HasFlag(WallState.Up))
             {
-                StartCoroutine(MakeStep(new Vector2(transform.position.x, transform.position.y + _mapCellSize), _speed));
+                StartMakingStep(new Vector2(transform.position.x, transform.position.y + _mapCellSize));
                 _currentPosInMap.y++;
             }
             else if (Input.GetButton("Down") && !_map[_currentPosInMap.x, _currentPosInMap.y].HasFlag(WallState.Down))
             {
-                StartCoroutine(MakeStep(new Vector2(transform.position.x, transform.position.y - _mapCellSize), _speed));
+                StartMakingStep(new Vector2(transform.position.x, transform.position.y - _mapCellSize));
                 _currentPosInMap.y--;
             }
+            
         }
 
     }
