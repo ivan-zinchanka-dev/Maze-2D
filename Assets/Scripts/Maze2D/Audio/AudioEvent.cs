d435a78308a1964a0e2be88a2b7f1b88c91aeaec
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-02-16 16:11:26 +0300

    refactor: settings

--- Assets/Scripts/Maze2D/Audio/AudioEvent.cs
+++ Assets/Scripts/Maze2D/Audio/AudioEvent.cs
@@ -1,23 +1,17 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using VContainer;
 
 namespace Maze2D.Audio
 {
-    [RequireComponent(typeof(Selectable))]
-    public class OnSubmitAudioEvent : MonoBehaviour, ISubmitHandler, IPointerClickHandler
+    public class AudioEvent : MonoBehaviour
     {
         [SerializeField] 
         private List<AudioClip> _audioClips;
         [Inject] 
         private AudioManager _audioManager;
-
-        public void OnSubmit(BaseEventData eventData) => PlayRandomClip();
-        public void OnPointerClick(PointerEventData eventData) => PlayRandomClip();
         
-        private void PlayRandomClip()
+        public void PlayOneShot()
         {
             if (_audioClips.Count > 0)
             {
--- Assets/Scripts/Maze2D/Audio/OnSelectAudioEvent.cs
+++ Assets/Scripts/Maze2D/Audio/OnSelectAudioEvent.cs
@@ -7,14 +7,17 @@ using VContainer;
 namespace Maze2D.Audio
 {
     [RequireComponent(typeof(Selectable))]
-    public class OnSelectAudioEvent : MonoBehaviour, ISelectHandler
+    public class OnSelectAudioEvent : MonoBehaviour, ISelectHandler, IPointerEnterHandler
     {
         [SerializeField] 
         private List<AudioClip> _audioClips;
         [Inject] 
         private AudioManager _audioManager;
         
-        public void OnSelect(BaseEventData eventData)
+        public void OnSelect(BaseEventData eventData) => PlayRandomClip();
+        public void OnPointerEnter(PointerEventData eventData) => PlayRandomClip();
+        
+        private void PlayRandomClip()
         {
             if (_audioClips.Count > 0)
             {
