a4bc9fef1d889ad324a5abb08e93badb5d2d8de3
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-02-20 22:07:10 +0300

    fix: audio event spam

--- Assets/Scripts/Maze2D/Audio/AudioEvent.cs
+++ Assets/Scripts/Maze2D/Audio/AudioEvent.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using VContainer;
+using Random = UnityEngine.Random;
 
 namespace Maze2D.Audio
 {
@@ -8,15 +11,29 @@ namespace Maze2D.Audio
     {
         [SerializeField] 
         private List<AudioClip> _audioClips;
+        [Space]
+        [SerializeField] [Range(0.15f, 1f)]
+        private float _cooldownSeconds = 0.15f;
+        
         [Inject] 
         private AudioManager _audioManager;
         
+        private TimeSpan _cooldown;
+        private UniTask _cooldownTask;
+        
+        private void Awake()
+        {
+            _cooldown = TimeSpan.FromSeconds(_cooldownSeconds);
+        }
+
         public void PlayOneShot()
         {
-            if (_audioClips.Count > 0)
+            if (_audioClips.Count > 0 && _cooldownTask.Status != UniTaskStatus.Pending)
             {
                 int randomIndex = Random.Range(0, _audioClips.Count);
                 _audioManager.PlayOneShot(_audioClips[randomIndex]);
+                
+                _cooldownTask = UniTask.Delay(_cooldown, DelayType.UnscaledDeltaTime);
             }
         }
     }
